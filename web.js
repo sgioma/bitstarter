b4a9753140bef85fc968b84a9ab653540e3185e4
Ubuntu <ubuntu@ip-172-31-23-36.us-west-2.compute.internal>
2013-07-22 22:45:25 +0000

    Changes for style

--- web.js
+++ web.js
@@ -12,7 +12,7 @@ var texto=contentBuffer.toString();
   response.send(texto);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
